93d551849f31ca69b504bcf9347b268fcc1ab10f
Weronika Malicka <weronikamarta.malicka@gmail.com>
2022-08-11 12:50:01 +0200

    Disabled scheduling

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -14,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 //@EnableSwagger2
 //@EnableWebMvc
-@EnableScheduling
+//@EnableScheduling
 public class CoreConfiguration {
 
     @Bean
